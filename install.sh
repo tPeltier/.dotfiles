e856f6bd1f293cc7da1a26464dd0d1b5cd1e9884
tPeltier <blackeagle393@gmail.com>
2022-05-25 04:54:43 +0000

    i3 config

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 # lvim
 
 mkdir -p "$HOME/.config/lvim" 
+mkdir -p "$HOME/.config/lvim/plugin"
 ln -sf "$HOME/.dotfiles/lvim/config.lua" "$HOME/.config/lvim"
 
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/.dotfiles/X11" "$HOME/.config"
+
+# i3
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/.dofiles/i3" "$HOME/.config"
