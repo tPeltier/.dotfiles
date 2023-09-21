dbb6e27aa51364f0d592124d80d949c9ab00ec26
justice <81177526+tPeltier@users.noreply.github.com>
2023-09-21 03:15:42 -0400

    fix(treesitter.lua): added langs

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "lua", "rust", "java", "markdown", "html", "sql", "php"},
+  ensure_installed = { "python", "lua",  "java", "markdown", "html", "css", "sql", "php", "yaml", "rust", "go", "c", "cpp"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
