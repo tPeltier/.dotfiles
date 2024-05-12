1a840f17c7b1f9ff7ebe452fa9ebbdc618257b4c
justice <81177526+tPeltier@users.noreply.github.com>
2024-05-12 16:49:29 -0400

    Enable spellcheck in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,6 +154,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Enable spellcheck
+vim.opt.spell = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
