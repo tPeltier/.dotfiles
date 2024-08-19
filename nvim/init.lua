00a9392efadbd2c4fa098b27c94b2275344960dc
justice <81177526+tPeltier@users.noreply.github.com>
2024-08-19 18:59:17 -0400

    set hybrid line numbers in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,9 +99,8 @@ vim.g.have_nerd_font = true
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
---vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
+-- Having both on gives hybrid line numbers
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
