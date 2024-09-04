d0f8e835f1c89d5b0c932ad4f2641711f4659af3
tPeltier <81177526+tPeltier@users.noreply.github.com>
2024-09-04 14:18:26 -0400

    add asm_lsp to init.lua
    adding assmebly lsp. it should handle masm as well?

--- nvim/init.lua
+++ nvim/init.lua
@@ -598,6 +598,7 @@ require("lazy").setup({
 				pyright = {},
 				marksman = {},
 				yamlls = {},
+				asm_lsp = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
