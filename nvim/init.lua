d6a0342945efb965bc74686aa9683cba3585231a
justice <81177526+tPeltier@users.noreply.github.com>
2024-09-12 00:11:14 -0400

    temp remove of sqls

--- nvim/init.lua
+++ nvim/init.lua
@@ -599,7 +599,7 @@ require("lazy").setup({
 				marksman = {},
 				yamlls = {},
 				asm_lsp = {},
-				sqls = {},
+				-- sqls = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
