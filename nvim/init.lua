7e0c7ea95267cbc35ccf21bcb95c482270f12114
justice <81177526+tPeltier@users.noreply.github.com>
2024-09-12 00:07:50 -0400

    add sqls lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -599,6 +599,7 @@ require("lazy").setup({
 				marksman = {},
 				yamlls = {},
 				asm_lsp = {},
+				sqls = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
