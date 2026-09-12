5a3359d26a9258fad2c3736555c07e696e1120a1
justice <81177526+tPeltier@users.noreply.github.com>
2026-09-12 16:33:56 -0400

    set nvim tabs => 2 instead of 4
    might need to adjust for somethings? idk

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -68,7 +68,7 @@ vim.opt.linebreak = true
 -- vim.opt.textwidth = 90
 
 -- Disable raw tabs
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
